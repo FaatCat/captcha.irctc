d0967634566998c1c3c38c17f2de6c88be27e0fe
Ubuntu <ubuntu@ip-172-31-15-78.ec2.internal>
2016-03-20 06:06:35 +0000

    changedls

--- main.lua
+++ main.lua
@@ -3,15 +3,37 @@ data = require 'data'
 dir = 'data/'
 seq_len, num_classes = 10, 36
 dataH, dataW = 120, 240
+training_set = 0.85
 
-print('creating data.t7..')
+--print('creating data.t7..')
 --X,Y = data.storeXY(dir,dataH,dataW,'captchaImage.')
 
-print('Loading images...')
-X,Y = data.loadXY(dir)
-print('Loaded ' .. X:size(1) .. ' images')
+--print('Loading images...')
+--X,Y = data.loadXY(dir)
+--print('Loaded ' .. X:size(1) .. ' images')
 
-Xt,Yt,Xv,Yv = data.split(X,Y,math.floor(X:size(1)*0.15))
+--Xt,Yt,Xv,Yv = data.split(X,Y,math.floor(X:size(1)*0.15))
+
+
+-- Split training data
+Y = data.loadY('data/')
+local dataNum = Y:size(1)
+print(dataNum .. ' data found.')
+
+local trainStart = 1
+local numTrain = math.floor(dataNum*training_set)
+local trainEnd = trainStart+numTrain-1
+local valStart = trainEnd+1
+local valEnd = dataNum
+
+print('TrainingData: ' .. trainStart .. ' to ' .. trainEnd)
+print('ValidationData: ' .. valStart .. ' to ' .. valEnd)
+
+Xt = data.loadXn(dir, 'captchaImage.', trainStart, trainEnd, dataH, dataW)
+Yt = Y.index(1,trainStart)
+Xv = data.loadXn(dir, 'captchaImage.', valStart, valEnd)
+Yv = Y.index(valStart, valEnd)
+
 --print(Yt[1])
 models = require 'models'
 net,ct = models.cnnModel(seq_len, num_classes, dataH,dataW)
@@ -37,7 +59,7 @@ print('Free memory: ' .. cutorch.getMemoryUsage())
 print('STARTING TRAINING...')
 
 for i=1,20 do
-   train.sgd(net,ct,Xt,Yt,Xv,Yv,1,sgd_config,batchSize)
+   train.sgd(net,ct,Xt,Yt,Xv,Yv,1,sgd_config,batchSize,dataH, dataW)
    
    torch.save('trained.t7', net)
    torch.save('ct.t7', ct)
